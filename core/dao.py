94fe7e8f8f0594f94ec04e9db9d30494de71f80f
frank-mes <mary15yyl@gmail.com>
2026-03-20 13:44:52 +0800

    Update dao.py

--- core/dao.py
+++ core/dao.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
+class WelderDAO:
+    def __init__(self):
+        # 初始化连接
+        self.conn = st.connection("gsheets", type=GSheetsConnection)
+
+    def select_all(self):
+        # ttl=0 确保不使用缓存，实时获取最新数据
+        return self.conn.read(ttl=0)
+
+    def update_sheet(self, df):
+        return self.conn.update(data=df)
