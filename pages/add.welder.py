01e83b71582221217df9e24e6efd98aae145368d
frank-mes <mary15yyl@gmail.com>
2026-03-20 14:43:37 +0800

    Update add.welder.py

--- pages/add.welder.py
+++ pages/add.welder.py
@@ -1,27 +1,18 @@
 import streamlit as st
-from core.controller import WelderController
 from core.entity import WelderEntity
+from core.controller import WelderController
 
-st.title("➕ 新增人员资质")
+st.set_page_config(page_title="新增人员")
 ctrl = WelderController()
 
-with st.form("add_form"):
-    c1, c2 = st.columns(2)
-    with c1:
-        name = st.text_input("姓名")
-        id_card = st.text_input("身份证号")
-        gender = st.selectbox("性别", ["男", "女"])
-        stamp_code = st.text_input("钢印号")
-    with c2:
-        workshop = st.text_input("车间")
-        team = st.text_input("产线/班组")
-        cert_large = st.text_input("大证")
-        cert_small = st.text_input("小证")
-    
-    if st.form_submit_button("提交数据"):
-        entity = WelderEntity(name=name, id_card=id_card, gender=gender, 
-                              stamp_code=stamp_code, workshop=workshop, 
-                              team=team, cert_large=cert_large, cert_small=cert_small)
-        success, msg = ctrl.create(entity)
+st.header("➕ 录入新焊工资质")
+with st.form("add_form", clear_on_submit=True):
+    # 表单布局... (此处同之前新增代码)
+    name = st.text_input("姓名")
+    id_card = st.text_input("身份证号")
+    # ...其他字段...
+    if st.form_submit_button("保存并入库"):
+        entity = WelderEntity(name=name, id_card=id_card, ...) 
+        success, msg = ctrl.dispatch_add(entity)
         if success: st.success(msg)
         else: st.error(msg)
