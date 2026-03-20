6e5dafa3450858d93e5007e0776b219ac9a39bc7
frank-mes <mary15yyl@gmail.com>
2026-03-20 14:42:56 +0800

    Update controller.py

--- core/controller.py
+++ core/controller.py
@@ -1,23 +1,17 @@
 from core.service import WelderService
-import pandas as pd
 
 class WelderController:
-    """控制器：接收前端指令并分发"""
     def __init__(self):
         self.service = WelderService()
 
-    def get_all(self):
-        try:
-            return self.service.list_all()
-        except Exception:
-            # 即使后端报错，也返回一个带表头的空表
-            return pd.DataFrame(columns=['id', 'name', 'gender', 'id_card', 'stamp_code', 'workshop', 'team', 'cert_large', 'cert_small'])
+    def dispatch_get_list(self, query=None):
+        return self.service.fetch_list(query)
 
-    def create(self, entity):
-        return self.service.add_process(entity)
+    def dispatch_add(self, entity):
+        return self.service.add_welder(entity)
 
-    def update(self, id_card, entity):
-        return self.service.update_process(id_card, entity)
+    def dispatch_update(self, id_card, entity):
+        return self.service.update_welder(id_card, entity)
 
-    def delete(self, id_card):
-        return self.service.delete_process(id_card)
+    def dispatch_delete(self, id_card):
+        return self.service.delete_welder(id_card)
