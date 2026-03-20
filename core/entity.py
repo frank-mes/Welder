bd481982a9b0550ccf6c01070b9309be2545ae71
frank-mes <mary15yyl@gmail.com>
2026-03-20 13:44:31 +0800

    Update entity.py

--- core/entity.py
+++ core/entity.py
@@ -1,1 +1,16 @@
+from dataclasses import dataclass, asdict
 
+@dataclass
+class WelderEntity:
+    id: str = ""
+    name: str = ""
+    gender: str = ""
+    id_card: str = ""
+    stamp_code: str = ""
+    workshop: str = ""
+    team: str = ""
+    cert_large: str = ""
+    cert_small: str = ""
+
+    def to_dict(self):
+        return asdict(self)
