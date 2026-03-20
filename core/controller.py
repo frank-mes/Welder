e08214ff31e6885fa53e21f9648ef25c819d5342
frank-mes <mary15yyl@gmail.com>
2026-03-20 14:01:02 +0800

    Update controller.py

--- core/controller.py
+++ core/controller.py
@@ -1,14 +1,18 @@
 from core.service import WelderService
 
 class WelderController:
+    """控制器：View 的唯一接口，不包含业务逻辑"""
     def __init__(self):
         self.service = WelderService()
 
-    def get_list(self):
-        return self.service.get_all_welders()
+    def get_all(self):
+        return self.service.list_all()
 
-    def add_welder(self, entity):
-        return self.service.create_welder(entity)
+    def create(self, entity):
+        return self.service.add_process(entity)
 
-    def remove_welder(self, id_card):
-        return self.service.delete_welder(id_card)
+    def update(self, id_card, entity):
+        return self.service.update_process(id_card, entity)
+
+    def delete(self, id_card):
+        return self.service.delete_process(id_card)
