ec957f01933d4b0b59ff040999e40b3d4361c6ef
frank-mes <mary15yyl@gmail.com>
2026-03-20 13:46:30 +0800

    Update controller.py

--- core/controller.py
+++ core/controller.py
@@ -1,1 +1,14 @@
+from core.service import WelderService
 
+class WelderController:
+    def __init__(self):
+        self.service = WelderService()
+
+    def get_list(self):
+        return self.service.get_all_welders()
+
+    def add_welder(self, entity):
+        return self.service.create_welder(entity)
+
+    def remove_welder(self, id_card):
+        return self.service.delete_welder(id_card)
