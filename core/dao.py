11767a51d0f6cb0a41b3320079ffe7aefe26a9b4
frank-mes <mary15yyl@gmail.com>
2026-03-20 15:27:52 +0800

    Update dao.py

--- core/dao.py
+++ core/dao.py
@@ -7,8 +7,10 @@ class WelderDAO:
         # 自动识别 secrets 中的 service_account 进行读写授权
         self.conn = st.connection("gsheets", type=GSheetsConnection)
 
-    def update_storage(self):
+    def select_all(self):
+        # ttl=0 确保 CRUD 后立即读取最新数据
         return self.conn.read(ttl=0)
 
-    def save_all(self, df: pd.DataFrame):
+    def update_storage(self, df: pd.DataFrame):
+        # 将原 save_all 改名为 update_storage，与 Service 层对接
         return self.conn.update(data=df)
